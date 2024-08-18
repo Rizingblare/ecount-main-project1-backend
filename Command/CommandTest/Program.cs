bbe752ce25ecbc44eda988e7b84ede6c250ca64c
Rizingblare <Rizingblare@gmail.com>
2024-08-18 19:11:15 +0900

    feat: CommandTest 프로젝트 추가

---
+++ Command/CommandTest/Program.cs
@@ -0,0 +1,32 @@
+﻿using Command;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CommandTest
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            var pipe = new PipeLine();
+            var createCmd = new CreateProdCommand();
+            var createReq = new CreateCommandRequest("HTTP 1.1 / POST", "Prod1 등록");
+
+            pipe.Register<CreateProdCommand, CommandResult>(createCmd)
+                .AddFilter((cmd) => {
+                    Console.WriteLine("# AddFilter 작동");
+                    return true;
+                })
+                .Mapping((cmd) => {
+                    Console.WriteLine("# Mapping 콜백 함수 클로저와 Command Request 매핑");
+                    cmd.Req = createReq;
+                })
+                .Executed((result) => Console.WriteLine("# result에 Executed 적용"));
+
+            pipe.Execute();
+        }
+    }
+}
