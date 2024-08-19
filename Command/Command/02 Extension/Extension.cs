ca33dc003dd1431b93d7e8c24a23332681d82ea9
Rizingblare <Rizingblare@gmail.com>
2024-08-19 20:07:34 +0900

    feat: 확장 클래스 추가

---
+++ Command/Command/02 Extension/Extension.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Command
+{
+    public static class CommonExtension
+    {
+        public static bool vIsNull<T>(this T obj)
+        {
+            return obj.Equals(null);
+        }
+
+        public static bool vIsEmpty<T>(this T obj)
+        {
+            return obj.Equals(null);
+        }
+
+        public static bool vIsNotEmpty<T>(this T obj)
+        {
+            return !obj.Equals(null);
+        }
+
+        public static bool vIsDefault<T>(this T obj)
+        {
+            return obj.Equals(null);
+        }
+    }
+    public static class CommandExtension
+    {
+        public static void AddError(this IExecutable executable, BaseError error)
+        {
+            executable.Errors.Add(error);
+        }
+    }
+}
