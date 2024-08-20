71325a94186608fe89bc722e0f035a2ddb90bfe9
Rizingblare <Rizingblare@gmail.com>
2024-08-20 17:58:43 +0900

    feat: 품목 업데이트 절반 설계

---
+++ Command/Command/04 Data/Dac/Product/UpdateProductDac.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Command
+{
+    public class UpdateProductDac : BaseCommand<>
+    {
+        public override void ExecuteCore()
+        {
+            var sql = @"
+                UPDATE flow.product_kjd
+                SET (@target_columns) = (@target_values)
+                WHERE com_code = @com_code and prod_cd = @prod_cd
+            ";
+
+            var parameters = new Dictionary<string, object>()
+            {
+                {"@com_code", this.Request.ComCode},
+                {"@prod_cd", this.Request.ProdCd},
+                {"@target_columns", this.Request.ComCode},
+                {"@target_values", this.Request.ComCode},
+            };
+        }
+    }
+
+    public class UpdateProductDacRequestDto : BaseRequest
+    {
+        public string ComCode { get; set; }
+        public string ProdCd { get; set; }
+        public string targetColumns { get; set; }
+        public string targetValues { get; set; }
+    }
+}
