fa2985d400002e9b8bfd637c1141916e1e233338
Rizingblare <Rizingblare@gmail.com>
2024-08-20 17:11:17 +0900

    feat: BaseEntity 구현

---
+++ Command/Command/04 Data/BaseEntity.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Command
+{
+    public class BaseEntity<TKey> : IEntity<TKey>
+        where TKey : IEntityKey, new()
+    {
+        private TKey _key;
+        public TKey Key
+        {
+            get
+            {
+                if (_key == null)
+                {
+                    _key = new TKey();
+                }
+                return _key;
+            }
+            set
+            {
+                _key = value;
+            }
+        }
+    }
+}
