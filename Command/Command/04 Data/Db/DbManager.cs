d826f45a96ef057475668f38a5e00326d4d126b7
Rizingblare <Rizingblare@gmail.com>
2024-08-20 17:12:01 +0900

    feat: DbManager 구현

---
+++ Command/Command/04 Data/Db/DbManager.cs
@@ -0,0 +1,79 @@
+﻿using Npgsql;
+using System;
+using System.Collections.Generic;
+using System.Data.Common;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Command
+{
+    public class DbManager
+    {
+        private const string _connectionString = "host=10.10.10.108;port=45111;username=ecount_user;password=1q2w3e4r;database=ecount";
+
+        public DbManager()
+        {
+
+        }
+
+        public int Execute(string sql, Dictionary<string, object> parameters)
+        {
+
+            // using var com = new NpgsqlConnection(_connectionString)
+            using (var conn = new NpgsqlConnection(_connectionString))
+            {
+                conn.Open();
+
+                using (var cmd = conn.CreateCommand())
+                {
+                    cmd.CommandText = sql;
+                    
+                    foreach (var param in parameters)
+                    {
+                        cmd.Parameters.Add(new NpgsqlParameter(param.Key, param.Value));
+                    }
+
+                    return cmd.ExecuteNonQuery(); // 영향받은 row의 수
+                }
+            }
+        }
+
+        //ActionDbDataReader : 여러 DBMS dbReader들의 상위 리더 
+        public List<T> Query<T>(string sql, Dictionary<string, object> parameters, Action<DbDataReader, T> mapper)
+        where T : new()
+        {
+            using (var conn = new NpgsqlConnection(_connectionString))
+            {
+                conn.Open();
+
+                using (var cmd = conn.CreateCommand())
+                {
+                    cmd.CommandText = sql;
+
+                    foreach (var param in parameters)
+                    {
+                        cmd.Parameters.Add(new NpgsqlParameter(param.Key, param.Value));
+                    }
+                    var result = new List<T>();
+                    // 서버 메모리 한계로 데이터를 한줄씩 읽기 위한 리더
+                    using (var reader = cmd.ExecuteReader()) {
+                        // 한줄씩 읽어옴. 마지막이면 false 반환
+                        while (reader.Read()) {
+                            var data = new T();
+                            //var a = reader("com_code");
+                            mapper(reader, data);
+                            result.Add(data);
+                        }
+                        return result;
+                    }
+                }
+            }
+        }
+
+        // Query : 여러건 조회
+        // Fetch : 단건 조회 (결과값이 하나)
+        // Execute : 실행 (Insert Update 등)
+        // Scalar : 단일값을 가져올때 (ex. row count 등)
+    }
+}
