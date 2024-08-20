0ff3ac4ce29b693d9bfc4948c07b9c2a155815ba
Rizingblare <Rizingblare@gmail.com>
2024-08-20 17:11:02 +0900

    feat: IEntity 인터페이스 구현

---
+++ Command/Command/04 Data/IEntity.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Command
+{
+    public interface IEntityKey
+    {
+
+    }
+    public interface IEntity<TKey>
+        where TKey : IEntityKey
+    {
+        TKey Key { get; }
+    }
+}
