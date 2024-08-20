85d19153ad701ce958c28c4af0951a4afa86eda7
Rizingblare <Rizingblare@gmail.com>
2024-08-20 17:12:54 +0900

    feat: Product와 Sale 엔티티 구현

---
+++ Command/Command/04 Data/Entity/Sale.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Command
+{
+    public class SaleKey : IEntityKey
+    {
+        public string COM_CODE { get; set; }
+        public string IO_DATE { get; set; }
+        public int IO_NO { get; set; }
+
+    }
+    public class Sale : BaseEntity<SaleKey>
+    {
+        public string PROD_CO {  get; set; }
+        public int QTY { get; set; }
+        public string REMARK { get; set; }
+    }
+}
